669734b12a0f166ba1476c2de67c36fcf8cda028
benjaminmodayil <benjaminmodayil@cedarville.edu>
2016-04-14 08:13:25 -0400

    Fixed 'FizzBuzz' problem

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
-	for (var i = 1; i <= 100; i++) {
-		if (i % 3 == 0 && i != 0) {
-			$('ul').append('<li> Fizz</li>');
-			// console.log("fizz");
-		}
-		else if (i % 5 == 0 && i != 0) {
-			$('ul').append('<li> Buzz</li>');
-			// console.log("buzz");
-		}
-		else {
-			$('ul').append('<li>' + i  + '</li>');
-			// console.log(i);
-		}
-	}
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            if (i % 5 == 0) {
+                $('ul').append('<li> FizzBuzz </li>');
+            } else {
+                $('ul').append('<li> Fizz</li>');
+            }
+
+        } else if (i % 5 == 0) {
+            if (i % 3 == 0) {
+                $('ul').append('<li> FizzBuzz </li>');
+            }
+	     else {
+                $('ul').append('<li> Buzz</li>');
+            }
+        }
+
+        else {
+            $('ul').append('<li>' + i + '</li>');
+        }
+    }
 })
